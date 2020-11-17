4b8336bc0c54cbbf0c392ee8d951097b41092fe7
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2020-11-17 15:28:45 -0300

    primeiro commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: '01.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '02.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '01.jpg', legenda: 'Prefeito' },
+                    { url: '02.jpg', legenda: 'Vice-Prefeito', small: true },
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '01.jpg', legenda: 'Prefeito' },
+                    { url: '02.jpg', legenda: 'Vice-Prefeito', small: true },
+                ]
+            },
+        ]
+    }
+];
